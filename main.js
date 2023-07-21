c8ea6a217176b6411cb3489b66480908f7a8ccde
hieutwendee <hieudd@twendeesoft.com>
2023-07-21 20:26:25 +0700

    add slider

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('slider__indicator').onclick = function(){
+    let btnList = document.querySelectorAll('.slider__indicator--btn');
+    let slideItem = document.querySelectorAll('.slider__indicator--btn');
+
+    console.log("🚀 ~ file: main.js:3 ~ document.getElementById ~ btnList:", btnList)
+    btnList.
+}
